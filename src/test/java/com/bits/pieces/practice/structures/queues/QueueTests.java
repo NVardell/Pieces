77bbdb5c7e54d7082ce3f32f3aa7ff8ef8920862
NV <nvardell@gmail.com>
2019-08-20 13:35:27 -0400

    Adding random queue tests

--- src/test/java/com/bits/pieces/practice/structures/queues/QueueTests.java
+++ src/test/java/com/bits/pieces/practice/structures/queues/QueueTests.java
@@ -1,12 +1,13 @@
 package com.bits.pieces.practice.structures.queues;
 
-//import static org.hamcrest.CoreMatchers.is;
-//import static org.hamcrest.MatcherAssert.assertThat;
-
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.concurrent.PriorityBlockingQueue;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 /**
  * TODO - Add Class Definition
@@ -19,4 +20,17 @@ public class QueueTests {
     Queue priorityQueue = new PriorityQueue();
     Queue linkedListQueue = new LinkedList();
     Queue blockingQueue = new PriorityBlockingQueue();
+
+    @Test
+    public void tempQueueTest() {
+        Queue<String> pq = new PriorityQueue<>();
+        pq.add("Awesome");
+        pq.add("Amazing");
+        assertThat(pq.poll(), is("Amazing"));
+
+        Queue<String> llq = new LinkedList<>();
+        llq.add("Awesome");
+        llq.add("Amazing");
+        assertThat(llq.poll(), is("Awesome"));
+    }
 }

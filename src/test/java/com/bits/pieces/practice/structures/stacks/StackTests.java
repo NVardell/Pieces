8d2eda0a5874cf2aa32b5cff67c3f09b9dbb8a2e
NV <nvardell@gmail.com>
2019-08-20 13:35:42 -0400

    Adding random stack tests

---
+++ src/test/java/com/bits/pieces/practice/structures/stacks/StackTests.java
@@ -0,0 +1,28 @@
+package com.bits.pieces.practice.structures.stacks;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+/**
+ * TODO - Add Class Definition
+ *
+ * @author Nate Vardell
+ * @since 8/17/2019
+ */
+public class StackTests {
+
+    @Test
+    public void tempStackTest() {
+        Stack<String> stack = new Stack<>();
+        stack.push("One");
+        stack.push("Two");
+        stack.push("Three");
+
+        assertThat(stack.search("Three"), is(1));
+        assertThat(stack.search("Two"), is(2));
+        assertThat(stack.search("Four"), is(-1));
+        assertThat(stack.empty(), is(false));
+        assertThat(stack.pop(), is("Three"));
+    }
+}

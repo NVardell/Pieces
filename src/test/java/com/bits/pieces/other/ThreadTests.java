fbb446aaefc80b8bcba8a1b094cacc90fe319039
NV <nvardell@gmail.com>
2019-09-05 18:56:10 -0400

    Pushing latest changes.

--- src/test/java/com/bits/pieces/other/ThreadTests.java
+++ src/test/java/com/bits/pieces/other/ThreadTests.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -22,7 +21,7 @@ import static org.hamcrest.core.Is.is;
  */
 public class ThreadTests {
 
-    @Test
+//    @Test
     public void givenMultiThread_whenNonSyncMethod() throws InterruptedException {
         ExecutorService service = Executors.newFixedThreadPool(2);
         SyncMethod summation = new SyncMethod();
@@ -33,15 +32,31 @@ public class ThreadTests {
 
     @Test
     public void givenMultiThread_whenSyncMethod() throws InterruptedException {
-        ExecutorService service = Executors.newFixedThreadPool(4);
+        ExecutorService service = Executors.newFixedThreadPool(1);
         SyncMethod summation = new SyncMethod();
 
         Instant start = Instant.now();
-        IntStream.range(0, 5000).forEach(count -> service.submit(summation::syncCalculate));
+        IntStream.range(0, 500000).forEach(count -> service.submit(summation::syncCalculate));
         Instant finish = Instant.now();
 
         service.awaitTermination(1000, TimeUnit.MILLISECONDS);
         assertThat(summation.getSum(), is(5000));
+
+        printElapsedTime(start, finish);
+    }
+
+    @Test
+    public void givenMultiThread_whenSyncMethod_2() throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        SyncMethod summation = new SyncMethod();
+
+        Instant start = Instant.now();
+        IntStream.range(0, 500000).forEach(count -> service.submit(summation::syncCalculate));
+        Instant finish = Instant.now();
+
+        service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+        assertThat(summation.getSum(), is(5000));
+
         printElapsedTime(start, finish);
     }
 
@@ -49,19 +64,16 @@ public class ThreadTests {
     private class SyncMethod {
         int sum = 0;
         private void calculate() { sum += 1; }
-        private synchronized void syncCalculate() { sum += 1;
-//            System.out.println("Thread's name: " + Thread.currentThread().getName());
+        private synchronized void syncCalculate() {
+            sum += 1;
+            System.out.println("Thread's name: " + Thread.currentThread().getName());
         }
     }
 
     private static void printElapsedTime(Instant start, Instant finish) {
-        System.out.println("INSTANT - NANOS = " + dateTimeDifference(start, finish, ChronoUnit.NANOS));
-        System.out.println("INSTANT - MICROS = " + dateTimeDifference(start, finish, ChronoUnit.MICROS));
-        System.out.println("INSTANT - MILLI = " + dateTimeDifference(start, finish, ChronoUnit.MILLIS));
-    }
-
-    private static long dateTimeDifference(Temporal d1, Temporal d2, ChronoUnit unit){
-        return unit.between(d1, d2);
+        System.out.println("INSTANT - NANOS = " + ChronoUnit.NANOS.between(start, finish));
+        System.out.println("INSTANT - MICROS = " + ChronoUnit.MICROS.between(start, finish));
+        System.out.println("INSTANT - MILLI = " + ChronoUnit.MILLIS.between(start, finish));
     }
 
 }

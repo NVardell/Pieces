d8a2d46bfe50e492d17e2d0ca1a2a17e94a34d78
NV <nvardell@gmail.com>
2019-09-11 14:07:59 -0400

    Pushing latest pieces.

--- src/test/java/com/bits/pieces/other/interviews/WeWork.java
+++ src/test/java/com/bits/pieces/other/interviews/WeWork.java
@@ -11,19 +11,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
  *      56874
  *      32698
  *      45683
- *      23487
+ *      23187
  *
  * Write a function that takes in an x & y coordinate
- * and return the sum of all of its neighbors.
+ * and return the sum of all its neighbors.
  *
  * NOTE : Only sum neighbor cells
- *
- * EXAMPLES
- *      0,0   ->   13
- *      1,1   ->   13
- *      2,2   ->   13
- *      3,3   ->   13
- *      4,4   ->   13
  *
  * @author Nate Vardell
  * @since 9/10/2019
@@ -35,7 +28,7 @@ public class WeWork {
             {5, 6, 8, 7, 4},
             {3, 2, 6, 9, 8},
             {4, 5, 6, 8, 3},
-            {2, 3, 4, 8, 7}
+            {2, 3, 1, 8, 7}
     };
 
     private static final int[] changeInX = new int[] {-1, 0, 1};
@@ -49,7 +42,7 @@ public class WeWork {
         assertThat(sumOfNeighbors(0, 0), is(13)); // 2 + 6 + 5 = 13
         assertThat(sumOfNeighbors(1, 1), is(27)); // 0 + 2 + 1 + 5 + 8 + 3 + 2 + 6 = 27
         assertThat(sumOfNeighbors(2, 2), is(51)); // 6 + 8 + 7 + 2 + 9 + 5 + 6 + 8 = 51
-        assertThat(sumOfNeighbors(3, 3), is(51)); // 6 + 9 + 8 + 6 + 3 + 4 + 8 + 7 = 51
+        assertThat(sumOfNeighbors(3, 3), is(48)); // 6 + 9 + 8 + 6 + 3 + 1 + 8 + 7 = 48
         assertThat(sumOfNeighbors(4, 4), is(19)); // 8 + 8 + 3 = 19
     }
 

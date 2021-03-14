0f62a710f7e1e75179701e4d5a333b8f349ed41d
NV <nvardell@gmail.com>
2021-03-14 18:33:46 -0400

    Pushing latest pieces.

--- src/test/java/com/bits/pieces/practice/structures/Tester.java
+++ src/test/java/com/bits/pieces/practice/structures/Tester.java
@@ -1,14 +1,16 @@
 package com.bits.pieces.practice.structures;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  *
  */
 public class Tester {
+
+    private static int[] start = new int[2];
+    private static int[] finish = new int[2];
 
     /**
      * Main Program Driver
@@ -22,10 +24,56 @@ public class Tester {
      * Iterate through each line of input.
      */
     private static void getGrid() throws IOException {
-        InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
-        BufferedReader in = new BufferedReader(reader);
-        String line = "asdf";
-        char c;
+        // InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
+        // BufferedReader in = new BufferedReader(reader);
 
+        // For each line in buffer - Add to String list.
+        //    - Use size of list to set Height Value
+        //    - Use String Length to set Width Value
+        Queue<String> lines = new LinkedList<>();
+        lines.add("###...##..");
+        lines.add("....#....#");
+        lines.add("#..###.#.#");
+        lines.add("S#.#.#.#..");
+        lines.add("...#...E#.");
+//        List<String> lines = Arrays.asList(
+//                "###...##..",
+//                "....#....#",
+//                "#..###.#.#",
+//                "S#.#.#.#..",
+//                "...#...E#.");
+
+        int l = lines.size();
+        System.out.println("Length = " + l);
+        assert lines.peek() != null;
+        int w = lines.peek().length();
+        System.out.println("Width = " + w);
+        char[][] grid = new char[l][w];
+
+        int x = 0;
+        while(lines.peek() != null) {
+            String line = lines.poll();
+            for (int y = 0; y < w; y++) {
+                char c = line.charAt(y);
+                grid[x][y] = c;
+
+                if(c=='S') {
+                    start[0]=x;start[1]=y;
+                } else if(c=='E')
+
+            }
+            x++;
+        }
+
+        printGrid(grid, l, w);
+
+    }
+
+    private static void printGrid(char[][] grid, int l, int w) {
+        for(int x=0; x<l; x++) {
+            for (int y = 0; y < w; y++)
+                System.out.print(grid[x][y]);
+            System.out.println();
+        }
     }
 }

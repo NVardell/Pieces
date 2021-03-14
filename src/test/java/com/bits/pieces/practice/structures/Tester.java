ca58d970ebd311ee161715ae1740a0d588c40ed8
NV <nvardell@gmail.com>
2021-03-14 15:27:48 -0400

    Pushing latest pieces.

---
+++ src/test/java/com/bits/pieces/practice/structures/Tester.java
@@ -0,0 +1,31 @@
+package com.bits.pieces.practice.structures;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+/**
+ *
+ */
+public class Tester {
+
+    /**
+     * Main Program Driver
+     */
+    public static void main(String[] args) throws IOException {
+        getGrid();
+    }
+
+
+    /**
+     * Iterate through each line of input.
+     */
+    private static void getGrid() throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
+        BufferedReader in = new BufferedReader(reader);
+        String line = "asdf";
+        char c;
+
+    }
+}

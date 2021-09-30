42b833a8491e938ec66404f96e2cd187c4df2f6d
NV <nvardell@gmail.com>
2021-09-30 16:34:39 -0400

    Added base template for easy sandboxing

---
+++ src/test/java/com/bits/pieces/topics/leet/_0_Playground.java
@@ -0,0 +1,24 @@
+package com.bits.pieces.topics.leet;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Sandbox for testing random shenanigans.
+ *
+ * @author NV
+ * @since ${DATE}
+ */
+public class _0_Playground {
+
+    @Test
+    public void tests() {
+        assertThat(solution(0), is(-1));
+    }
+
+    public int solution(int x) {
+        return -1;
+    }
+}

697cd2aa6770b973b39d68e863492baeb5d97716
NV <nvardell@gmail.com>
2021-12-29 11:34:16 -0500

    Fixed HackerRank Solution class errors

--- src/test/java/com/bits/pieces/topics/interviews/Amazon_2021_1.java
+++ src/test/java/com/bits/pieces/topics/interviews/Amazon_2021_1.java
@@ -58,46 +58,49 @@ public class Amazon_2021_1 {
     public void testSearch() {
         List<String> input = Arrays.asList("bags", "baggage", "banner", "box", "cloths");
         assertThat(searchSuggestions(input, "customer").size(), is(4));
-
     }
 
-//    public class Solution {
-//        public static void main(String[] args) throws IOException {
-//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-//
-//            int repositoryCount = Integer.parseInt(bufferedReader.readLine().trim());
-//
-//            List<String> repository = IntStream.range(0, repositoryCount).mapToObj(i -> {
-//                        try {
-//                            return bufferedReader.readLine();
-//                        } catch (IOException ex) {
-//                            throw new RuntimeException(ex);
-//                        }
-//                    })
-//                    .collect(toList());
-//
-//            String customerQuery = bufferedReader.readLine();
-//
-//            List<List<String>> result = Result.searchSuggestions(repository, customerQuery);
-//
-//            result.stream()
-//                    .map(
-//                            r -> r.stream()
-//                                    .collect(joining(" "))
-//                    )
-//                    .map(r -> r + "\n")
-//                    .collect(toList())
-//                    .forEach(e -> {
-//                        try {
-//                            bufferedWriter.write(e);
-//                        } catch (IOException ex) {
-//                            throw new RuntimeException(ex);
-//                        }
-//                    });
-//
-//            bufferedReader.close();
-//            bufferedWriter.close();
-//        }
-//    }
+
+    /**
+     * HackerRank Generated Runner Class
+     */
+    public class Solution {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+            int repositoryCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+            List<String> repository = IntStream.range(0, repositoryCount).mapToObj(i -> {
+                        try {
+                            return bufferedReader.readLine();
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    })
+                    .collect(toList());
+
+            String customerQuery = bufferedReader.readLine();
+
+            List<List<String>> result = searchSuggestions(repository, customerQuery);
+
+            result.stream()
+                    .map(
+                            r -> r.stream()
+                                    .collect(joining(" "))
+                    )
+                    .map(r -> r + "\n")
+                    .collect(toList())
+                    .forEach(e -> {
+                        try {
+                            bufferedWriter.write(e);
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    });
+
+            bufferedReader.close();
+            bufferedWriter.close();
+        }
+    }
 }

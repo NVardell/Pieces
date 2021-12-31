b35b93aac4e764c82020583080fd5ef4512b5206
NV <nvardell@gmail.com>
2021-12-31 17:41:35 -0500

    Added Code Assessment Template for Amazon Code Assessment Qs

--- src/test/java/com/bits/pieces/topics/interviews/_2021/Amazon3.java
+++ src/test/java/com/bits/pieces/topics/interviews/_2021/Amazon3.java
@@ -1,24 +1,41 @@
-package com.bits.pieces.topics.interviews._2021;/**
- * NAME - 
- * TOPICS - 
- * COMPLEX - 
- * URL - 
+package com.bits.pieces.topics.interviews._2021;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * NAME -
+ * TOPICS -
+ * COMPLEX -
+ * URL -
  *
  * PROBLEM
- *      - 
+ *      -
  *
  * NOTES
- *      - 
+ *      -
  *
  * CONSTRAINTS
- *      - 
+ *      -
  *
  * EXAMPLE #1
- *      In ~ 
- *      Out ~ 
- *      Note ~ 
+ *      In ~
+ *      Out ~
+ *      Note ~
  *
  * @author NV
  * @since 12/30/2021
- */public class Amazon3 {
+ */
+public class Amazon3 {
+
+    @Test
+    public void tests() {
+        assertThat(solution(0), is(-1));
+    }
+
+    public int solution(int x) {
+        return -1;
+    }
 }
--- src/test/java/com/bits/pieces/topics/interviews/_2021/Amazon4.java
+++ src/test/java/com/bits/pieces/topics/interviews/_2021/Amazon4.java
@@ -1,17 +1,24 @@
-package com.bits.pieces.topics.interviews._2021;/**
+package com.bits.pieces.topics.interviews._2021;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
  * NAME -
  * TOPICS -
  * COMPLEX -
  * URL -
  *
  * PROBLEM
- *      - 
+ *      -
  *
  * NOTES
- *      - 
+ *      -
  *
  * CONSTRAINTS
- *      - 
+ *      -
  *
  * EXAMPLE #1
  *      In ~
@@ -20,5 +27,15 @@ package com.bits.pieces.topics.interviews._2021;/**
  *
  * @author NV
  * @since 12/30/2021
- */public class Amazon4 {
+ */
+public class Amazon4 {
+
+    @Test
+    public void tests() {
+        assertThat(solution(0), is(-1));
+    }
+
+    public int solution(int x) {
+        return -1;
+    }
 }

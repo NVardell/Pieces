a4bdb86c91de29657ea21fc840fb1ddc24eeefaa
NV <nvardell@gmail.com>
2021-12-31 17:39:58 -0500

    Reformatted

--- src/test/java/com/bits/pieces/topics/temp/Temp.java
+++ src/test/java/com/bits/pieces/topics/temp/Temp.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 /**
  * TODO - Add Class Definition
@@ -22,11 +23,9 @@ import java.util.Random;
 public class Temp {
 
 
-
-
     @Test
-    public void testSearchRange(){
-        System.out.println(Arrays.toString(searchRange(new int[]{5,7,7,8,8,10}, 7)));
+    public void testSearchRange() {
+        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 7)));
     }
 
     int low = -1;
@@ -38,8 +37,8 @@ public class Temp {
     }
 
     @Test
-    public void testInts(){
-        int[] ia = new int[] {5,7,7,8,8,10};
+    public void testInts() {
+        int[] ia = new int[]{5, 7, 7, 8, 8, 10};
         int half = ia.length / 2;
         System.out.println(ia.length);
         System.out.println(half);
@@ -51,7 +50,7 @@ public class Temp {
     }
 
     @Test
-    public void testString(){
+    public void testString() {
         String temp = "asdf-adsf-";
         System.out.println(temp);
         temp = temp.trim();
@@ -67,7 +66,9 @@ public class Temp {
 
     @Test
     public void testAdd_2() {
-        List<Integer> indexes = new ArrayList(){{add(2);}};
+        List<Integer> indexes = new ArrayList() {{
+            add(2);
+        }};
         indexes.add(2);
         System.out.println(indexes);
     }
@@ -81,58 +82,26 @@ public class Temp {
 
     @Test
     public void random() {
-        List<Integer> indexes = new ArrayList(){{add(2); add(4);}};
-        indexes.add(6); indexes.add(17);
-        indexes.add(632); indexes.add(18);
-        indexes.add(12); indexes.add(19);
-        indexes.add(234); indexes.add(20);
-        indexes.add(61); indexes.add(21);
-        indexes.add(63); indexes.add(22);
-        indexes.add(12); indexes.add(23);
-        indexes.add(11);
-        indexes.add(14);
-        indexes.add(15);
-        indexes.add(16);
-        System.out.println(indexes);
-        int randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
-        randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
-        randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
-        randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
-        randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
-        randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
-        randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
-        randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
-        randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
-        randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
-        randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
-        randomIndex = new Random().nextInt(indexes.size());
-        System.out.println(randomIndex);
+        List<Integer> indexes = Arrays.asList(2, 4, 6, 17, 632, 18, 12, 19, 234, 21, 61, 21, 63, 22, 12, 23, 11, 14, 15, 16);
+
+        IntStream.range(0, indexes.size()).forEach( x ->
+                System.out.println(indexes.get(new Random().nextInt(indexes.size())))
+        );
     }
 
     @Test
-    public void testRepeatingString(){
+    public void testRepeatingString() {
         String str = "helloslkhellodjladfjhello";
         String findStr = "hello";
         int lastIndex = 0;
         int count = 0;
 
-        while(lastIndex != -1){
+        while (lastIndex != -1) {
 
-            lastIndex = str.indexOf(findStr,lastIndex);
+            lastIndex = str.indexOf(findStr, lastIndex);
 
-            if(lastIndex != -1){
-                count ++;
+            if (lastIndex != -1) {
+                count++;
                 lastIndex += findStr.length();
             }
         }

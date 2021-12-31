99989178f7a6a8e58f4ab46d38fcb8653df9b1af
NV <nvardell@gmail.com>
2021-12-31 12:04:37 -0500

    Adding template file for Amazon code assessment

---
+++ src/test/java/com/bits/pieces/topics/interviews/_2021/Amazon4.java
@@ -0,0 +1,24 @@
+package com.bits.pieces.topics.interviews._2021;/**
+ * NAME -
+ * TOPICS -
+ * COMPLEX -
+ * URL -
+ *
+ * PROBLEM
+ *      - 
+ *
+ * NOTES
+ *      - 
+ *
+ * CONSTRAINTS
+ *      - 
+ *
+ * EXAMPLE #1
+ *      In ~
+ *      Out ~
+ *      Note ~
+ *
+ * @author NV
+ * @since 12/30/2021
+ */public class Amazon4 {
+}

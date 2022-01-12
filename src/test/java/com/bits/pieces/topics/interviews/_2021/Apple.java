907a853d355fcc334944006847babeecf3bf8002
NV <nvardell@gmail.com>
2022-01-12 18:52:09 -0500

    Prep template for Apple code assessment

---
+++ src/test/java/com/bits/pieces/topics/interviews/_2021/Apple.java
@@ -0,0 +1,16 @@
+package com.bits.pieces.topics.interviews._2021;
+
+/**
+ * NAME -
+ * COMPANY -
+ * NOTES -
+ * CONSTRAINTS -
+ * EXAMPLE #1     In ~     Out ~     Note ~
+ * import static org.hamcrest.MatcherAssert.assertThat;
+ * import static org.hamcrest.Matchers.is;
+ *
+ * @author NV
+ * @since 1/11/2022
+ */
+public class Apple {
+}
